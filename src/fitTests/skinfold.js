7edde2c351276b61c352b3bd0186940e2b2c8346
Danilo Heraclio <emaildedanilo@gmail.com>
2020-01-12 20:56:48 -0500

    added pollock skinfold equation JS

---
+++ src/fitTests/skinfold.js
@@ -0,0 +1,40 @@
+/* Siri Equation
+** % Body Fat = (495 / Body Density) - 450.
+*/
+function siriEquation(bd) {
+   return (495 / bd) - 450;
+}
+
+/* MALE
+** sos -> sum of skinfold 
+** ( Chest, Axilla, Tricep, Subscapular, Abdominal, Suprailiac and Thigh )
+** measured in mm
+** (reference: Jackson, A.S. & Pollock, M.L. (1978), based on a sample aged 18-61)
+*/
+export const pollockMale7skinfold = (sos, age) => {
+   const bodyDensity = 1.112
+                     - (0.00043499 * sos)
+                     + (0.00000055 * Math.pow(sos, 2))
+                     - (0.00028826 * age);
+
+   const bodyPercentage = siriEquation(bodyDensity);
+
+   return (Math.round(bodyPercentage * 100) / 100).toFixed(2);
+};
+
+/* FEMALE
+** sos -> sum of skinfold 
+** ( Chest, Axilla, Tricep, Subscapular, Abdominal, Suprailiac and Thigh )
+** measured in mm
+** (reference: Jackson, et al. (1980), based on a sample aged 18-55)
+*/
+export const pollockFemale7skinfold = (sos, age) => {
+   const bodyDensity = 1.097
+                     - (0.00046971 * sos)
+                     + (0.00000056 * Math.pow(sos, 2))
+                     - (0.00012828 * age);
+
+   const bodyPercentage = siriEquation(bodyDensity);
+
+   return (Math.round(bodyPercentage * 100) / 100).toFixed(2);
+}
